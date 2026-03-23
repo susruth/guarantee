cd825f1b87aeeea0e0d295815fd5383356fe782a
susruth <susruth@susruth.com>
2026-03-24 02:12:07 +0530

    fixed logo size

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [react()],
   adapter: cloudflare(),
 });
